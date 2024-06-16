6d3b1fef5c3ee8f0d75760e51bcd93eb04946859
babifukuoka@gmail.com <babifukuoka@gmail.com>
2024-06-16 19:53:12 +0900

    files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Cards from './components/Cards.js';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Memory Game</h1>
+     <Cards />
+    </div>
+  );
+}
+
+export default App;
